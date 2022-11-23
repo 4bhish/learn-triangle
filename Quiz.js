e97763433c8df5819ec05f6424529579ba8ded46
4bhish <108976136+4bhish@users.noreply.github.com>
2022-11-23 21:28:39 +0530

    Update html Pages

--- Quiz.js
+++ Quiz.js
@@ -1,7 +1,20 @@
 btnSubmit = document.querySelector("#submit")
 formInfo = document.querySelector("#form-data")
+outputDiv = document.querySelector("#score")
+
+const correctAns = ["90", "right angled"];
 
 btnSubmit.addEventListener("click",function submitHandler(){
     const formResults  = new FormData(formInfo);
-    console.log(formResults)
+    let i=0;
+    let score = 0;
+    for(let value of formResults.values(formInfo))
+    {
+        if(value === correctAns[i])
+        {
+            score++;
+        }
+        i++;
+    }
+    outputDiv.innerText =(`You have scored ${score}/2`);
 })
