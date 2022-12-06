eaba9d489cb7d68149ee050e31985fdd60419b2e
4bhish <108976136+4bhish@users.noreply.github.com>
2022-12-06 23:09:07 +0530

    Update isTriangle.js

--- scripts/isTriangle.js
+++ scripts/isTriangle.js
@@ -9,12 +9,20 @@ function addAngle(a,b,c)
 }
 
 btnCheck.addEventListener("click",function clickHandle(){
-   const angles = addAngle( Number(angleInputs[0].value),Number(angleInputs[1].value),Number(angleInputs[2].value) );
-   if(angles === 180)
-   {
-    outputDiv.innerText = ("Congrats ! That forms the Triangle.🤩");
-   } else
-   {
-    outputDiv.innerText = ("We are sorry to inform that does not form The Triangle.🥲");
-   }
+  
+    if(angleInputs[0].value && angleInputs[1].value && angleInputs[2].value )  {
+    const angles = addAngle( Number(angleInputs[0].value),Number(angleInputs[1].value),Number(angleInputs[2].value) );
+    if(angles === 180)
+    {
+     outputDiv.innerText = ("Congrats ! That forms the Triangle.🤩");
+    } else
+    {
+     outputDiv.innerText = ("We are sorry to inform that does not form The Triangle.🥲");
+    }
+  } else
+  {
+    outputDiv.innerText = ("Make sure you have filled on angles🥲");
+  }
 })
+
+   
