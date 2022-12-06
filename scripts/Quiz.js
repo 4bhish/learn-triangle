7067c4ef474631d027e12e08bacaa8c98c277239
4bhish <108976136+4bhish@users.noreply.github.com>
2022-12-06 18:13:24 +0530

    Update Quiz.js

--- scripts/Quiz.js
+++ scripts/Quiz.js
@@ -2,7 +2,7 @@ btnSubmit = document.querySelector("#submit")
 formInfo = document.querySelector("#form-data")
 outputDiv = document.querySelector("#score")
 
-const correctAns = ["90", "right angled"];
+const correctAns = ["90", "right angled", "Equilateral","(√3/2)a2","3","60 cm","10 cm"];
 
 btnSubmit.addEventListener("click",function submitHandler(){
     const formResults  = new FormData(formInfo);
